4e2174be1e8cd24dae5a34a36761405ce87461b4
kperilla <kevin.m.perilla@gmail.com>
2022-12-02 10:22:40 -0600

    Complete part 2

--- src/day1/day1.rs
+++ src/day1/day1.rs
@@ -1,18 +1,12 @@
-use std::fs;
+use std::{fs, vec};
 
 const INPUT_PATH: &str = "src/day1/input";
 
-fn get_input(path: &str) -> String {
-    let input = fs::read_to_string(INPUT_PATH)
-        .expect("Should have been able to read the file");
-    return input;
-}
 
 pub fn part1() {
     println!("Part 1");
-    let input = get_input(INPUT_PATH);
-    // let input = fs::read_to_string(INPUT_PATH)
-    //     .expect("Should have been able to read the file");
+    let input = fs::read_to_string(INPUT_PATH)
+        .expect("Should have been able to read the file");
     let split_input = input.trim().split("\n\n");
     let list_of_list_of_nums = split_input
         .map(|x| x.split("\n")
@@ -24,4 +18,16 @@ pub fn part1() {
 
 pub fn part2(){
     println!("Part 2");
+    let input = fs::read_to_string(INPUT_PATH)
+        .expect("Should have been able to read the file");
+    let split_input = input.trim().split("\n\n");
+    let list_of_list_of_nums = split_input
+        .map(|x| x.split("\n")
+                        .map(|x| x.parse::<u32>().unwrap()));
+    let list_of_sums = list_of_list_of_nums.map(|x| x.sum::<u32>());
+    let mut sum_vector: Vec<u32> = list_of_sums.collect();
+    sum_vector.sort();
+    let vector_len = sum_vector.len();
+    let sum = sum_vector[vector_len - 1] + sum_vector[vector_len - 2] + sum_vector[vector_len - 3];
+    println!("Sum of top 3: {}", sum);
 }
