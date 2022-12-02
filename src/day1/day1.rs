dc04b881ba4c8ef6e9ed28866bb11cc2e1151b13
kperilla <kevin.m.perilla@gmail.com>
2022-12-02 09:47:48 -0600

    Complete part 1

--- src/day1/day1.rs
+++ src/day1/day1.rs
@@ -1,42 +1,18 @@
-// use error_chain::error_chain;
-// use std::io::Read;
-// use std::error::Error;
 use std::fs;
 
 const INPUT_PATH: &str = "src/day1/input";
 
-// error_chain! {
-//     foreign_links {
-//         Io(std::io::Error);
-//         HttpRequest(reqwest::Error);
-//     }
-// }
-
-// TODO: I couldn't figure out async stuff so come back when I can
-// async fn get_input(part: &str) {
-    // match reqwest::get("https://httpbin.org/ip").await {
-    //     Ok(mut response) => {
-    //         if response.status() == reqwest::StatusCode::OK {
-    //             match response.text().await {
-    //                 Ok(text) => println!("Response: {}", text),
-    //                 Err(_) => println!("Could not read response text")
-    //             }
-    //         } else {
-    //             println!("Request error");
-    //         }
-    //     }
-    //     Err(_) => {
-    //         println!("Could not make request")
-    //     }
-    // }
-    // let result = reqwest::get("https://httpbin.org/ip").await;
-    // Ok(result);
-// }
+fn get_input(path: &str) -> String {
+    let input = fs::read_to_string(INPUT_PATH)
+        .expect("Should have been able to read the file");
+    return input;
+}
 
 pub fn part1() {
     println!("Part 1");
-    let input = fs::read_to_string(INPUT_PATH)
-        .expect("Should have been able to read the file");
+    let input = get_input(INPUT_PATH);
+    // let input = fs::read_to_string(INPUT_PATH)
+    //     .expect("Should have been able to read the file");
     let split_input = input.trim().split("\n\n");
     let list_of_list_of_nums = split_input
         .map(|x| x.split("\n")
